55140a1d2ac83a15c638627a773dcf9b3c33f47d
F88 <685250+F88@users.noreply.github.com>
2025-08-25 22:11:01 +0900

    fix: update deploy script in package.json for correct base path and adjust base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // base: '/yonourawa/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
