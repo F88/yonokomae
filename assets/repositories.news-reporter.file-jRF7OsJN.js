37e10d8c752caef0367568413b4b99c502b7ca69
F88 <685250+F88@users.noreply.github.com>
2025-09-03 09:16:35 +0900

    Updates --skip-ci

--- assets/repositories.news-reporter.file-jRF7OsJN.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./delay-utils-lugK4fzg.js";import{l as r}from"./battle-seed-loader-1C_-eQ7d.js";import"./config-r6H3Wq70.js";import"./index-BvQox0Ct.js";class p{delay;constructor(e){this.delay=e?.delay}async generateReport(e){return await t(this.delay,e?.signal),r({roots:["/seeds/news/","/src/seeds/news/"]})}}export{p as NewsReporterFileiBattleReportRepository};
