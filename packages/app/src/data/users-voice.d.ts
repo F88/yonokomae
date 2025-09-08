102fe17c5f4dcdcb6c99f7c014584e3041086143
F88 <685250+F88@users.noreply.github.com>
2025-09-08 11:08:34 +0900

    feat(export): enhance battle directory loading with multiple candidate paths

--- packages/app/src/data/users-voice.d.ts
+++ packages/app/src/data/users-voice.d.ts
@@ -2,8 +2,8 @@
  * 架空の「ユーザーの声」を記録します
  */
 export declare const USER_VOICES: {
-    name: string;
-    age: string;
-    voice: string;
+  name: string;
+  age: string;
+  voice: string;
 }[];
 //# sourceMappingURL=users-voice.d.ts.map
